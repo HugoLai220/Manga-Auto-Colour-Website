fcd657b8596c024b1efb05aa02ca31bf4da5e47d
Lai Chun Ho <23020364D@connect.polyu.hk>
2025-04-22 16:26:40 +0800

    Update uploadManager.js

--- js/uploadManager.js
+++ js/uploadManager.js
@@ -1,10 +1,11 @@
 const uploadManager = {
-    simulateFileUpload: function () {
-        alert('In a real application, you can upload an image from your own device.');
 
-        // Set a default original image
+    simulateFileUpload: function () {
+        // This is just a simulation, no actual file upload happens
+        alert('In a real application, here will open file selector for you to upload an image.');
+
         setTimeout(() => {
-            document.getElementById('originalImage').src = 'image/B&W input.png';
+            document.getElementById('originalImage').src = '';
         }, 500);
     }
 };
